482a21a48541e5755fc16ce917c5d322e6b520bb
Bhaskar Jyoti Das <bdas@brandinstitute.com>
2022-03-04 19:26:46 +0530

    firstMessage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {  FormGroup, Validators,FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent implements OnInit{
+    title='Reactive Forms';
+    myForm:any= FormGroup
+    constructor() { }
+  ngOnInit() { 
+    this.myForm = new FormGroup({
+     userName:new FormControl('',[Validators.required]),
+     email:new FormControl('',[Validators.required,Validators.email]),
+     password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+     mobile:new FormControl('', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+    }); 
+    }
+    get userName(){
+      return this.myForm.get('userName');
+    }
+    get email(){
+      return this.myForm.get('email');
+    }
+    get password(){
+      return this.myForm.get('password');
+    }
+    get mobile(){  return this.myForm.get('mobile');}
+    onSubmit()  { console.log(this.myForm.controls);}
+  }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
